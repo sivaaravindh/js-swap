70d6c128f52379452b81b8bc6bf52f86dde41ad2
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-15 02:26:03 -0800

    index

---
+++ main.js
@@ -0,0 +1,19 @@
+var a,b,c;
+ a=parseInt (prompt("Enter the value"));
+ b=parseInt (prompt("Enter the value"));
+
+
+
+function swap(){
+
+    c=a;
+    a=b;
+    b=c;
+       
+    document.write(a + "<br>");
+    document.write(b);
+    
+}
+
+swap()
+     
